9eccca3d3ecff9738f1e8c53e94f61d35724727a
Krinal Gami <104003095+Krinal09@users.noreply.github.com>
2025-04-20 04:34:12 +0530

    Update products-controller.js

--- controllers/shop/products-controller.js
+++ controllers/shop/products-controller.js
@@ -2,16 +2,29 @@ const Product = require("../../models/Product");
 
 const getFilteredProducts = async (req, res) => {
   try {
-    const { category = [], brand = [], sortBy = "price-lowtohigh" } = req.query;
+    const { 
+      category = [], 
+      brand = [], 
+      sortBy = "price-lowtohigh",
+      page = 1,
+      limit = 12
+    } = req.query;
 
     let filters = {};
 
+    // More efficient filtering using $and to combine multiple conditions
+    const conditions = [];
+    
     if (category.length) {
-      filters.category = { $in: category.split(",") };
+      conditions.push({ category: { $in: category.split(",") } });
     }
 
     if (brand.length) {
-      filters.brand = { $in: brand.split(",") };
+      conditions.push({ brand: { $in: brand.split(",") } });
+    }
+
+    if (conditions.length > 0) {
+      filters.$and = conditions;
     }
 
     let sort = {};
@@ -19,38 +32,47 @@ const getFilteredProducts = async (req, res) => {
     switch (sortBy) {
       case "price-lowtohigh":
         sort.price = 1;
-
         break;
       case "price-hightolow":
         sort.price = -1;
-
         break;
       case "title-atoz":
         sort.title = 1;
-
         break;
-
       case "title-ztoa":
         sort.title = -1;
-
         break;
-
       default:
         sort.price = 1;
         break;
     }
 
-    const products = await Product.find(filters).sort(sort);
+    // Add pagination
+    const skip = (page - 1) * limit;
+    
+    // Get total count for pagination
+    const total = await Product.countDocuments(filters);
+    
+    // Get paginated products
+    const products = await Product.find(filters)
+      .sort(sort)
+      .skip(skip)
+      .limit(Number(limit));
 
     res.status(200).json({
       success: true,
       data: products,
+      pagination: {
+        total,
+        page: Number(page),
+        pages: Math.ceil(total / limit)
+      }
     });
   } catch (e) {
-    console.log(error);
+    console.log(e);
     res.status(500).json({
       success: false,
-      message: "Some error occured",
+      message: "An error occurred while fetching products"
     });
   }
 };
@@ -60,21 +82,22 @@ const getProductDetails = async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
 
-    if (!product)
+    if (!product) {
       return res.status(404).json({
         success: false,
-        message: "Product not found!",
+        message: "Product not found!"
       });
+    }
 
     res.status(200).json({
       success: true,
-      data: product,
+      data: product
     });
   } catch (e) {
-    console.log(error);
+    console.log(e);
     res.status(500).json({
       success: false,
-      message: "Some error occured",
+      message: "An error occurred while fetching product details"
     });
   }
 };
