cb3db2b7461ba8036e2c664332a7660cc5ad9589
Krinal Gami <104003095+Krinal09@users.noreply.github.com>
2025-04-21 00:43:49 +0530

    Add files via upload

---
+++ models/FeatureImage.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const featureImageSchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("FeatureImage", featureImageSchema); 
